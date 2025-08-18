0fa523f2361c7920e820ef1cee26d816043d87c5
Hamza Ali <hamzaali.codes@gmail.com>
2025-08-18 15:47:17 +0500

    book-navigatin using keys

--- src/Components/BookPage/Book.jsx
+++ src/Components/BookPage/Book.jsx
@@ -1,6 +1,6 @@
 
 import HTMLFlipBook from 'react-pageflip';
-import React,{ useRef } from 'react';
+import React,{ useRef,useEffect } from 'react';
 
 function Image(){
     return(
@@ -21,9 +21,27 @@ const Page = React.forwardRef((props, ref) => {
 });
 
 function MyBook() {
-    
+    const bookRef = useRef(null);
+
+        useEffect(() => {
+    const handleKeyDown = (e) => {
+        if (!bookRef.current) return;
+
+        if (e.key === "ArrowRight") {
+        bookRef.current.pageFlip().flipNext();
+        }
+        if (e.key === "ArrowLeft") {
+        bookRef.current.pageFlip().flipPrev(); 
+        }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+    }, []);
+
     return (
-        <HTMLFlipBook showCover={true} width={500} height={550} maxHeight={550} minHeight={550} maxWidth={500} minWidth={500} size='stretch'  drawShadow={true}
+        <HTMLFlipBook ref={bookRef} showCover={true} width={500} height={550} maxHeight={550} minHeight={550} maxWidth={500} minWidth={500} size='stretch'  drawShadow={true}
   usePortrait={false} className="shadow-xl ">
             <Page> <img className='w-full h-full object-cover' src='/Book-cover.png'/> </Page>
             <Page> <img className='w-full h-full object-cover' src='/Credits.png'/> </Page>
